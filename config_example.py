c7c9fbea5f654eb6514734c967d153d138dac31d
Christian Bosshard <cbosshard28@gmail.com>
2023-07-10 22:13:55 +0200

    changed immo url with parameters

---
+++ config_example.py
@@ -0,0 +1,14 @@
+roomNumber = "4.5"
+locationId = "4147"
+radius = "10"
+immoscoutLink = f"https://rest-api.immoscout24.ch/v4/en/properties?l={locationId}&nrf={roomNumber}&pt=3t&r={radius}&s=1&t=1&inp=1"
+mongoDBUrl = ""
+discordWebhook = ""
+delay = 120
+
+sendContactRequest = False
+fullname= "Max Muster"
+email = "test.test@gmail.com"
+phoneNr = "+41857234234"
+
+apiKeyGoogle = ""
